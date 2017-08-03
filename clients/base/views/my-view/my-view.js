e69f002cae3d041edfacfcc1fadb634af8980a45
Syed Muhammad Taha <syed.taha@RT-LT-227.rolustech.net>
2017-08-03 20:42:09 +0500

    custom Api

--- clients/base/views/my-view/my-view.js
+++ clients/base/views/my-view/my-view.js
@@ -1,19 +1,13 @@
 ({
-    className: 'tcenter',
-    loadData: function (options) {
-        //populate your data
-        myData=new Object();
-        myData.myProperty = "My Value";
-        this.myData = myData;
-        /*
-            //alternatively, you can pass in a JSON array to populate your data
-            myData = $.parseJSON( '{"myData":{"myProperty":"My Value"}}' );
-            _.extend(this, myData);
-        */
-        //reset flags on reload
-        if (options && _.isFunction(options.complete)) {
-            options.complete();
-        }
+    className: 'my-view tcenter',
+    cubeOptions: {
+        spin: false
+    },
+    events: {
+        'click .sugar-cube': 'spinCube'
+    },
+    spinCube: function() {
+        this.cubeOptions.spin = !this.cubeOptions.spin;
         this.render();
     }
 })
