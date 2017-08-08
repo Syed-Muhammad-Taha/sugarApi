fd2447ceef09bb8441c6036c79cfbc1144f03363
Syed Muhammad Taha <syed.taha@RT-LT-227.rolustech.net>
2017-08-08 11:07:40 +0500

    Last Push of Probation

--- clients/base/views/my-view/my-view.js
+++ clients/base/views/my-view/my-view.js
@@ -1,13 +1,38 @@
 ({
-    className: 'my-view tcenter',
-    cubeOptions: {
-        spin: false
-    },
-    events: {
-        'click .sugar-cube': 'spinCube'
-    },
-    spinCube: function() {
-        this.cubeOptions.spin = !this.cubeOptions.spin;
+    className: 'tcenter',
+    loadData: function (options) {
+        //populate your data
+        myData=new Object();
+        myData.myProperty = "My Value2";
+	myData.heading = "Opportunity Reports";
+        this.myData = myData;
+	console.log(this);
+	app.api.call('get',App.api.buildURL('customApi/getClosedWon'), null, {
+           success: _.bind(function(response) {
+		myDat=new Object();
+		myDat.string="Looping";
+		console.log(this);
+		myData.arr=response;
+		myData.heading = "Opportunity Reportses";
+	  	this.myData = myData;
+               for (var index in response) {
+		   console.log(response[index]);
+               }
+           },this),
+           error: function() {
+               console.log("could not get data from " + apiUrl);
+           }
+       });
+	
+        /*
+            //alternatively, you can pass in a JSON array to populate your data
+            myData = $.parseJSON( '{"myData":{"myProperty":"My Value"}}' );
+            _.extend(this, myData);
+        */
+        //reset flags on reload
+        if (options && _.isFunction(options.complete)) {
+            options.complete();
+        }
         this.render();
     }
 })
